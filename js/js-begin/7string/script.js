fd0264aa5c3a4fb317ed6ad6b85865b5e9892817
handongyexingren <1340551309@qq.com>
2021-09-27 21:19:09 +0800

    string

---
+++ js/js-begin/7string/script.js
@@ -0,0 +1,12 @@
+console.log('front' + 234);
+console.log('19' + '47');
+
+let s = '123'
+console.log(typeof s);
+s = Number(s);
+console.log(typeof s);
+
+let n = 123;
+console.log(typeof n);
+n = s.toString();
+console.log(typeof n);
