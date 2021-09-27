fa97c6e48a45ab2886a42a6b5e0d64017c438cab
handongyexingren <1340551309@qq.com>
2021-09-27 23:30:49 +0800

    字符串操作，silly-story-generator

---
+++ js/js-begin/10silly-story-generator/script.js
@@ -0,0 +1,43 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+let storyText = '今天气温 34 摄氏度，:inserta:出去遛弯。\
+当走到:insertb:门前时，突然就:insertc:。人们都惊呆了，李雷\
+全程目睹但并没有慌，因为:inserta:是一个 130 公斤的胖子，天气又辣么热。';
+
+let insertX = ['怪兽威利','大老爹','圣诞老人'];
+let insertY = ['肯德基','迪士尼乐园','白宫'];
+let insertZ = ['自燃了','在人行道化成了一坨泥','变成一条鼻涕虫爬走了'];
+
+randomize.addEventListener('click', result);
+
+function result() {
+    let newStory = storyText;
+    if(customName.value !== '') {
+        let name = customName.value;
+        newStory = newStory.replace('李雷',name);
+    }
+    let xItem = randomValueFromArray(insertX);
+    let yItem = randomValueFromArray(insertY);
+    let zItem = randomValueFromArray(insertZ);
+    // replace每次只会替换一个
+    newStory = newStory.replace(':inserta:',xItem);
+    newStory = newStory.replace(':inserta:',xItem);
+    newStory = newStory.replace(':insertb:',yItem);
+    newStory = newStory.replace(':insertc:',zItem);
+
+    if(document.getElementById("american").checked) {
+        let weight = Math.round(300);
+        let temperature = Math.round(94);
+        newStory = newStory.replace(34,temperature)
+        newStory = newStory.replace(130,weight)
+    }
+
+    story.textContent = newStory;
+    story.style.visibility = 'visible';
+}
