7c9f2c91e5521655ff86e3b807a148510db505f1
handongyexingren <1340551309@qq.com>
2021-09-28 15:05:28 +0800

    events

--- js/building-blocks/7events/script.js
+++ js/building-blocks/7events/script.js
@@ -8,3 +8,10 @@ btn.onclick = function() {
     let color = 'rgb('+random(255)+','+random(255)+','+random(255)+')';
     document.body.style.backgroundColor = color;
 }
+
+function random1(e) {
+    color = 'rgb('+random(255)+','+random(255)+','+random(255)+')';
+    e.target.style.backgroundColor = color;
+}
+
+btn.addEventListener('click',random1);
